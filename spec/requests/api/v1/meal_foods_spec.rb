da0c4b148d6aabfadc4fc8a8dae31400146f12c1
JamisonOrdway <jamisonordway@gmail.com>
2018-08-15 16:23:10 -0600

    Tests for delete food from meal functionality

--- spec/requests/api/v1/meal_foods_spec.rb
+++ spec/requests/api/v1/meal_foods_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe "Meal foods api" do
     post "/api/v1/meals/#{@meal_1.id}/foods/#{@food_1.id}"
     expect(MealFood.last.meal_id).to eq(@meal_1.id)
     expect(MealFood.last.food_id).to eq(@food_1.id)
+    expect(response.status).to eq(201)
   end
   it 'returns a 404 error if meal is not found' do
     post "/api/v1/meals/#{@meal_1.id + 1}/foods/#{@food_1.id}"
@@ -30,5 +31,35 @@ RSpec.describe "Meal foods api" do
 
     expect(response.status).to eq(404)
     expect(response.body).to eq({ error: 'This food could not be found. Please try again' }.to_json)
+  end
+  it 'can delete specified food from the specified meal' do
+    meal_foods = MealFood.create!(meal: @meal_1, food: @food_1)
+
+    delete "/api/v1/meals/#{@meal_1.id}/foods/#{@food_1.id}"
+
+    expect(response).to be_successful
+    meal = Meal.find(@meal_1.id)
+
+    expect(meal.foods).to eq([])
+  end
+  it 'removes the MealFood record which connects the specified meal and food' do
+    meal_food = MealFood.create!(meal: @meal_1, food: @food_1)
+
+    delete "/api/v1/meals/#{@meal_1.id}/foods/#{@food_1.id}"
+    expect { MealFood.find(meal_food.id) }.to raise_exception(ActiveRecord::RecordNotFound)
+  end
+  it 'returns a 404 if the food is not found' do
+    meal_food = MealFood.create!(meal: @meal_1, food: @food_1)
+
+    delete "/api/v1/meals/#{@meal_1.id}/foods/#{@food_1.id + 1}"
+    expect(response.status).to eq(404)
+    expect(response.body).to eq({ error: 'This meal could not be found. Please try again'}.to_json)
+  end
+  it 'returns a 404 if the meal is not found' do
+    meal_food = MealFood.create!(meal: @meal_1, food: @food_1)
+
+    delete "/api/v1/meals/#{@meal_1.id + 1}/foods/#{@food_1.id}"
+    expect(response.status).to eq(404)
+    expect(response.body).to eq({ error: 'This food could not be found. Please try again'}.to_json)
   end
 end
