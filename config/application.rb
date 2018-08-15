c7154ec63c4703169ac2fd253238557e6539a787
JamisonOrdway <jamisonordway@gmail.com>
2018-08-15 17:46:49 -0600

    changes application controller to inherit from Actioncontroller::API

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 
 module QsApi
   class Application < Rails::Application
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
@@ -37,6 +38,5 @@ module QsApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
   end
 end
