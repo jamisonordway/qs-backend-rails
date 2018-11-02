a3158e944fb48553933b123373466a4d78c53c99
JamisonOrdway <jamisonordway@gmail.com>
2018-11-02 15:40:41 -0600

    CORS WIP

--- config/application.rb
+++ config/application.rb
@@ -25,18 +25,9 @@ module QsApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins ['http://localhost:3000', 'https://qs-rails-backend.herokuapp.com']
         resource '*', headers: :any, methods: :any
       end
     end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
   end
 end
